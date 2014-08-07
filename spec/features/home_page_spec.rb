20d79fb6175d5228a97f2926a11f0b5b1ac7dea1
Derrick Lannaman <derricklannaman@gmail.com>
2014-08-06 21:11:04 -0400

    add visit home page

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -2,8 +2,14 @@
 #   As a visitor
 #   I want to visit the home page
 #   so I can learn more about the website
-
 # Scenario: Visit the Home page
 #   Given I am a visitor
 #   when I visit the home page
 #   I should see the text 'Cultural Cuisine Discovery'
+
+describe 'Visiting the home page' do
+  it 'should show the content Cultural Cuisine Discovery' do
+    visit '/'
+    expect(page).to have_content 'Cultural Cuisine Discovery'
+    end
+end
