870891e652ee57894356ae63d4be12ecce81cc50
Derrick Lannaman <derricklannaman@gmail.com>
2014-08-07 15:18:05 -0400

    add test for home page links

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -7,9 +7,24 @@
 #   when I visit the home page
 #   I should see the text 'Cultural Cuisine Discovery'
 
+# Scenario:
+#   when I visit the Home page
+#     I can learn more about the company by
+#     clicking links to get more information
+
 describe 'Visiting the home page' do
   it 'should show the content Cultural Cuisine Discovery' do
     visit '/'
     expect(page).to have_content 'Cultural Cuisine Discovery'
     end
+
+  it "shows links to a 'how it works', about, and contact page" do
+    visit '/'
+    expect(page).to have_link '/how-it-works'
+    expect(page).to have_link '/about'
+    expect(page).to have_link 'contact'
+  end
+
 end
+
+
