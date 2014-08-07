08a1e51a1cc3bb2994e50b761d7b52a9f3479d59
Derrick Lannaman <derricklannaman@gmail.com>
2014-08-06 20:44:58 -0400

    format .rspec, suppress test framework file generation, add user story, remove test files

---
+++ spec/features/home_page_spec.rb
@@ -0,0 +1,9 @@
+# Feature: Home Page
+#   As a visitor
+#   I want to visit the home page
+#   so I can learn more about the website
+
+# Scenario: Visit the Home page
+#   Given I am a visitor
+#   when I visit the home page
+#   I should see the text 'Cultural Cuisine Discovery'
