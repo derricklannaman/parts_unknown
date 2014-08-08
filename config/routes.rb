400c4568991f252cec62e500db4c190b26ab1813
Derrick Lannaman <derricklannaman@gmail.com>
2014-08-08 13:35:38 -0400

    change layout/application to haml, refactored top level navigation, add destinations link

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
 
-  root 'welcome#index'
 
-  get '/how_it_works', to: 'welcome#how', as: 'how_it_works'
-  get '/about', to: 'welcome#about', as: 'about'
-  get '/contact', to: 'welcome#contact', as: 'contact'
+# Visitor accessible pages
+  root 'welcome#index'
+  get '/how_it_works',  to: 'welcome#how',      as: 'how_it_works'
+  get '/about',         to: 'welcome#about',    as: 'about'
+  get '/contact',       to: 'welcome#contact',  as: 'contact'
+
+
+  resources :destinations, only: :index
+
+
+
 end
--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -12,36 +12,51 @@
 #     I can learn more about the company by
 #     clicking links to get more information
 
+# Scenario:
+#   When I visit the Home page
+#     I can choose a 'destination' by clicking
+#     the destination button
 
 describe 'Visiting the home page' do
   it 'should show the content Cultural Cuisine Discovery' do
-    visit '/'
+    visit root_path
     expect(page).to have_content 'Cultural Cuisine Discovery'
     end
 
   it "shows links to a 'how it works', about, and contact page" do
-    visit '/'
+    visit root_path
     expect(page).to have_link 'how it works'
     expect(page).to have_link 'about'
     expect(page).to have_link 'contact'
   end
 
   it "clicking 'how it works' link goes to how_it_works page" do
-    visit '/'
+    visit root_path
     click_link 'how it works'
     expect(current_path).to eq(how_it_works_path)
   end
 
   it "clicking 'about' link goes to about page" do
-    visit '/'
+    visit root_path
     click_link 'about'
-    expect(current_path).to eq('/about')
+    expect(current_path).to eq(about_path)
   end
 
   it "clicking 'contact' link goes to the contact page" do
-    visit '/'
+    visit root_path
     click_link 'contact'
-    expect(current_path).to eq('/contact')
+    expect(current_path).to eq(contact_path)
+  end
+
+  it 'will show a button to select a destination' do
+    visit root_path
+    expect(page).to have_selector('a.destination-btn')
+  end
+
+  it 'clicking the destination link goes to the destination page' do
+    visit root_path
+    click_link 'Choose Destination'
+    expect(current_path).to eq('/destinations')
   end
 
 
