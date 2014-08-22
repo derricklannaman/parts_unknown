716a67358e2445b351feec43668b08a53b7f65fd
Derrick Lannaman <derricklannaman@gmail.com>
2014-08-22 18:58:57 -0400

    fix db.yaml, tidy some code

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
 
-  $('.next-arrow').hide();
+  // $('#map3d').hide();
 
-  $('div.learn-more').hover(showNextArrow, hideNextArrow);
+  // $('div.learn-more').hover(showNextArrow, hideNextArrow);
 
 
 });
 
 
-function showNextArrow() {
-  $this = $(this);
-  $(this).find('img.next-arrow').first().fadeIn(200);
-}
+// function showNextArrow() {
+//   $this = $(this);
+//   $(this).find('img.next-arrow').first().fadeIn(200);
+// }
 
-function hideNextArrow() {
-  $this = $(this);
-  $(this).find('img.next-arrow').first().fadeOut(200);
-}
+// function hideNextArrow() {
+//   $this = $(this);
+//   $(this).find('img.next-arrow').first().fadeOut(200);
+// }
