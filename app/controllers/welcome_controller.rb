7cbe0840af0fa598dba763bc7531a8b45eac0ca4
Derrick Lannaman <derricklannaman@gmail.com>
2014-08-22 19:18:33 -0400

    fix error in production page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
 
   def index
     @locale = Destination.where(name: 'Havana').first
-    @featured_dish = @locale.meals.first
+    # @featured_dish = @locale.meals.first
   end
 
   def how
