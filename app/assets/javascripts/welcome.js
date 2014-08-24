538490067bc6f3b4fa724407d1c737df23036740
Derrick Lannaman <derricklannaman@gmail.com>
2014-08-23 21:57:38 -0400

    first pass at google earth mashup

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,11 +1,29 @@
 $(document).ready(function(){
 
-  // $('#map3d').hide();
+  $('#map3d').addClass('invisible');
 
-  // $('div.learn-more').hover(showNextArrow, hideNextArrow);
-
+  $('.destination').on('click', showEarth);
 
 });
+
+function showEarth() {
+  $this = $(this);
+  var places = $('.destination');
+  var clicked_id = $this.attr('href').split('/')[2];
+
+  for(var i = 0; i < places.length; i++) {
+    var place = $(places[i]);
+    var place_id = place.attr('href').split('/')[2];
+    if ( place_id != clicked_id) {
+      place.fadeOut(500);
+      $('#map3d').delay(400).fadeTo(500, 1, function(){
+        $this.delay(1000).fadeOut(500);
+        // console.log($this);
+      });
+    }
+  }
+}
+
 
 
 // function showNextArrow() {
